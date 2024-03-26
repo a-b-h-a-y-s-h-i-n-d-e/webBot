558ae54281ad813b8cc7da8665af9f23fce2881e
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-03-27 00:04:20 +0530

    new dir for static css files now!

---
+++ scripts/techcrunch.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+
